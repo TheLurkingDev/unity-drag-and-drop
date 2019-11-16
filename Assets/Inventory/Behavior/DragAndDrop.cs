58e57b7356694de369d413ff4cb44f2f3d1b836e
Darren Cook <thelurkingdev@gmail.com>
2019-11-16 00:15:09 -0500

    Added CanvasGroup component and referenced within DragAndDrop script. Add InventorySlot.cs and setup to receive Drop event.

--- Assets/Inventory/Behavior/DragAndDrop.cs
+++ Assets/Inventory/Behavior/DragAndDrop.cs
@@ -1,11 +1,12 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class DragAndDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
+public class DragAndDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler, IDropHandler
 {
     [SerializeField]
     private Canvas _canvas;
     private RectTransform _rectTransform;
+    private CanvasGroup _canvasGroup;
 
     private void Start()
     {
@@ -13,12 +14,16 @@ public class DragAndDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler
         {
             throw new System.Exception("You must add the Canvas to the DragAndDrop script component.");
         }
+
         _rectTransform = GetComponent<RectTransform>();
+        _canvasGroup = GetComponent<CanvasGroup>();
     }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        Debug.Log("You started dragging me...");        
+        Debug.Log("You started dragging me...");
+        _canvasGroup.blocksRaycasts = false;
+        _canvasGroup.alpha = 0.6f;
     }
 
     public void OnDrag(PointerEventData eventData)
@@ -30,10 +35,17 @@ public class DragAndDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler
     public void OnEndDrag(PointerEventData eventData)
     {
         Debug.Log("You stopped dragging me...");
+        _canvasGroup.blocksRaycasts = true;
+        _canvasGroup.alpha = 1f;
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
         Debug.Log("You clicked me!");
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
     }
 }
---
+++ Assets/Inventory/Behavior/InventorySlot.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InventorySlot : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("You dropped something on me!");
+    }
+}
