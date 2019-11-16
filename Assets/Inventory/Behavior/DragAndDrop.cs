a03026e941aac710f244a136aef8b2323b213be7
Darren Cook <thelurkingdev@gmail.com>
2019-11-15 23:53:58 -0500

    Added interfaces used for dragging UI objects.

--- Assets/Inventory/Behavior/DragAndDrop.cs
+++ Assets/Inventory/Behavior/DragAndDrop.cs
@@ -1,8 +1,23 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class DragAndDrop : MonoBehaviour, IPointerDownHandler
+public class DragAndDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("You started dragging me...");
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("You are still dragging me...");
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("You stopped dragging me...");
+    }
+
     public void OnPointerDown(PointerEventData eventData)
     {
         Debug.Log("You clicked me!");
