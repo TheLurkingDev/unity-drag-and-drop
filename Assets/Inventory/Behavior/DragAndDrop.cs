099aa38c0fd05bed323dd96fbe11e140da48a0d2
Darren Cook <thelurkingdev@gmail.com>
2019-11-15 23:44:49 -0500

    Added DragAndDrop.cs.

---
+++ Assets/Inventory/Behavior/DragAndDrop.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragAndDrop : MonoBehaviour, IPointerDownHandler
+{
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("You clicked me!");
+    }
+}
