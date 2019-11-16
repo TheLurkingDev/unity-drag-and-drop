a95603a21df647394072dc222c8f24a65f847f41
Darren Cook <thelurkingdev@gmail.com>
2019-11-16 00:02:51 -0500

    Implemented drag and drop behavior.

--- Assets/Inventory/Behavior/DragAndDrop.cs
+++ Assets/Inventory/Behavior/DragAndDrop.cs
@@ -3,14 +3,28 @@ using UnityEngine.EventSystems;
 
 public class DragAndDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
+    [SerializeField]
+    private Canvas _canvas;
+    private RectTransform _rectTransform;
+
+    private void Start()
+    {
+        if(_canvas == null)
+        {
+            throw new System.Exception("You must add the Canvas to the DragAndDrop script component.");
+        }
+        _rectTransform = GetComponent<RectTransform>();
+    }
+
     public void OnBeginDrag(PointerEventData eventData)
     {
-        Debug.Log("You started dragging me...");
+        Debug.Log("You started dragging me...");        
     }
 
     public void OnDrag(PointerEventData eventData)
     {
         Debug.Log("You are still dragging me...");
+        _rectTransform.anchoredPosition += eventData.delta / _canvas.scaleFactor;
     }
 
     public void OnEndDrag(PointerEventData eventData)
