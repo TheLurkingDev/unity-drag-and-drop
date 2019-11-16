13b1cfe45a2621cde567257e23bf405a19926d5f
Darren Cook <thelurkingdev@gmail.com>
2019-11-16 00:20:35 -0500

    Added behavior to reposition DragAndDrop object to center of InventorySlot when OnDrop is called.

--- Assets/Inventory/Behavior/InventorySlot.cs
+++ Assets/Inventory/Behavior/InventorySlot.cs
@@ -6,5 +6,10 @@ public class InventorySlot : MonoBehaviour, IDropHandler
     public void OnDrop(PointerEventData eventData)
     {
         Debug.Log("You dropped something on me!");
+        if(eventData.pointerDrag != null)
+        {
+            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition =
+                GetComponent<RectTransform>().anchoredPosition;
+        }
     }
 }
